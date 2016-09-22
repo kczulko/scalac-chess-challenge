f826f43ec6cc38a3cadf1a66b9dd7a1bd662a9c4
kczulko <karol.czulkowski@gmail.com>
2016-09-22 17:58:13 +0200

    Add pieces classes: King, Bishop, Rook, Knight

--- src/main/scala/model/Piece.scala
+++ src/main/scala/model/Piece.scala
@@ -2,6 +2,12 @@ package model
 
 abstract class Piece(position: Position)
 
+case class King(p: Position) extends Piece(p)
+
 case class Queen(p: Position) extends Piece(p)
 
+case class Bishop(p: Position) extends Piece(p)
 
+case class Rook(p: Position) extends Piece(p)
+
+case class Knight(p: Position) extends Piece(p)
