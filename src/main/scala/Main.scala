1382c32a5e63f380bc659a5b71b2898bc1584d02
kczulko <karol.czulkowski@gmail.com>
2016-09-25 00:19:18 +0200

    Refactored cli args parser code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,53 +1,47 @@
-import scopt.OptionDef
-
-case class Args(rows: Int = 0,
-                cols: Int = 0,
-                queens: Int = 0,
-                rooks: Int = 0,
-                knights: Int = 0,
-                kings: Int = 0,
-                bishops: Int = 0)
 
 object Main {
+  private[this] case class Args(rows: Int = 0,
+                  cols: Int = 0,
+                  queens: Int = 0,
+                  rooks: Int = 0,
+                  knights: Int = 0,
+                  kings: Int = 0,
+                  bishops: Int = 0)
 
-  def withNumberValidator(failurePredicate: Int => Boolean, failureMessage: String): (Int) => Either[String, Unit] = {
-    case i if failurePredicate(i) => Left(failureMessage)
-    case _ => Right()
+  private[this] class ArgsParser extends scopt.OptionParser[Args]("Scalac's chess challenge") {
+    private[this] def withNumberValidator(failurePredicate: Int => Boolean, failureMessage: String): (Int) => Either[String, Unit] = {
+      case i if failurePredicate(i) => failure(failureMessage)
+      case _ => success
+    }
+
+    private[this] def createDimOpt(dimName: String)(copyFun: (Int, Args) => Args) =
+      opt[Int](dimName)
+        .validate { withNumberValidator(_ < 0, s"${dimName} must be > 0") }
+        .text(s"amount of chessboard $dimName")
+        .action(copyFun)
+        .required()
+
+    private[this] def createPieceOpt(pieceName: String)(copyFun: (Int, Args) => Args) =
+      opt[Int](pieceName)
+        .validate { withNumberValidator(_ < 0, s"${pieceName} number must be >= 0") }
+        .text(s"amount of ${pieceName}")
+        .action { copyFun }
+        .optional()
+
+    createDimOpt("rows")((v, args) => args.copy(rows = v))
+    createDimOpt("cols")((v, args) => args.copy(cols = v))
+    createPieceOpt("kings")((v, args) => args.copy(kings = v))
+    createPieceOpt("queens")((v, args) => args.copy(queens = v))
+    createPieceOpt("bishops")((v, args) => args.copy(bishops = v))
+    createPieceOpt("rooks")((v, args) => args.copy(rooks = v))
+    createPieceOpt("knights")((v, args) => args.copy(knights = v))
   }
 
   def main(args: Array[String]) {
+    val parser = new ArgsParser
 
-    val parser = new scopt.OptionParser[Args]("Scalac's chess challenge") {
-      def createPieceOpt(pieceName: String)(copyFun: (Int, Args) => Args): OptionDef[Int, Args] = {
-        opt[Int](pieceName)
-          .validate { withNumberValidator(_ < 0, s"${pieceName} number must be >= 0") }
-          .action { copyFun }
-          .optional()
-          .text(s"amount of ${pieceName}")
-      }
-
-      opt[Int]("rows")
-        .validate { withNumberValidator(_ <= 0, "rows must be greater than 0") }
-        .action((v, args) => args.copy(rows = v))
-        .text("number of chessboard rows")
-        .required()
-
-      opt[Int]("cols")
-        .validate { withNumberValidator(_ <= 0, "cols must be greater than 0") }
-        .action((v, args) => args.copy(cols = v))
-        .text("number of chessboard cols")
-        .required()
-
-      createPieceOpt("queens")((v, args) => args.copy(queens = v))
-      createPieceOpt("rooks")((v, args) => args.copy(rooks = v))
-      createPieceOpt("knights")((v, args) => args.copy(knights = v))
-      createPieceOpt("knights")((v, args) => args.copy(knights = v))
-      createPieceOpt("kings")((v, args) => args.copy(kings = v))
-      createPieceOpt("bishops")((v, args) => args.copy(bishops = v))
-    }
-
-    parser.parse(Seq("--rows", "3", "--cols", "2", "--queens", "-4"), Args()) match {
-      case Some(args) => println(args)
+    parser.parse(args, Args()) match {
+      case Some(arguments) => println(arguments)
       case _ =>
     }
   }
