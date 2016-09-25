90d82c3e06e9ed283dacb89f5b99cb7acd36057d
kczulko <karol.czulkowski@gmail.com>
2016-09-25 21:36:05 +0200

    Added tailrec algoritm implementation.

--- src/main/scala/algorithm/Solver.scala
+++ src/main/scala/algorithm/Solver.scala
@@ -4,7 +4,7 @@ import model.Piece.Candidate
 import model.{Chessboard, Dim}
 
 class Solver {
-  def findSolutions(piecesToPlace: List[Candidate], dim: Dim): Seq[Chessboard] = {
+  def findSolutions1(piecesToPlace: List[Candidate], dim: Dim): Seq[Chessboard] = {
     def loop(solutions: List[Chessboard], candidates: List[Candidate], currentChessboard: Chessboard): Seq[Chessboard] =
       candidates match {
         case first :: others => {
@@ -18,5 +18,23 @@ class Solver {
       }
 
     loop(List(), piecesToPlace, Chessboard(Set(), dim)).distinct
+  }
+
+  def findSolutions(piecesToPlace: List[Candidate], dim: Dim): Seq[Chessboard] = {
+    def toNextLevelResults(candidate: Candidate, chessboard: Chessboard): Seq[Chessboard] =
+      chessboard.unaffectedPositions
+        .map(candidate(_))
+        .filter(chessboard canPlace _)
+        .map(chessboard place _)
+
+    def loop(intermediateResults: List[Chessboard], candidates: List[Candidate]): Seq[Chessboard] = candidates match {
+      case first :: others => {
+        val nextLevelResults = intermediateResults.flatMap(toNextLevelResults(first, _))
+        loop(nextLevelResults, others)
+      }
+      case _ => intermediateResults.distinct
+    }
+
+    loop(List(Chessboard(Set(), dim)), piecesToPlace)
   }
 }
