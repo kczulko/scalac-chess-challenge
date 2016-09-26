3213080b0ac435896b7be7eb7fd253172ca77168
Karol Czulkowski <karol.czulkowski@intel.com>
2016-09-26 18:29:44 +0200

    Profiled Solver. Added parallel collections and changed List::distinct to Set implementation which assures uniqueness by default. Removed 'distinct' call bottleneck. Changed 'filterNot' and 'map' calls chain to single 'collect' partial function with some scala sugar.

--- src/main/scala/algorithm/Solver.scala
+++ src/main/scala/algorithm/Solver.scala
@@ -4,24 +4,28 @@ import model.Piece.Candidate
 import model.{Chessboard, Dim}
 
 import scala.annotation.tailrec
+import scala.collection.parallel.ParSet
 
 class Solver {
   def findSolutions(piecesToPlace: List[Candidate], dim: Dim): Seq[Chessboard] = {
     def toNextLevelResults(candidate: Candidate, chessboard: Chessboard): Seq[Chessboard] =
       chessboard.unaffectedPositions
         .map(candidate(_))
-        .filterNot(_.attacksAnyOf(chessboard.pieces))
-        .map(chessboard place _)
+        .collect({
+          case thisPiece if thisPiece attacksAnyOf chessboard.pieces equals false => chessboard place thisPiece
+        })
 
     @tailrec
-    def loop(intermediateResults: List[Chessboard], candidates: List[Candidate]): Seq[Chessboard] = candidates match {
+    def loop(intermediateResults: ParSet[Chessboard], candidates: List[Candidate]): ParSet[Chessboard] = candidates match {
       case first :: others => {
-        val nextLevelResults = intermediateResults.flatMap(toNextLevelResults(first, _)).distinct
+        println(s"Starting iteration. There are still ${others.length} iterations left...")
+        val nextLevelResults = intermediateResults.flatMap(toNextLevelResults(first, _))
+        println(s"Iteration finished. Processed ${intermediateResults.size} chessboards.")
         loop(nextLevelResults, others)
       }
       case _ => intermediateResults
     }
 
-    loop(List(Chessboard(Set(), dim)), piecesToPlace)
+    loop(ParSet(Chessboard(Set(), dim)), piecesToPlace).seq.toSeq
   }
 }
